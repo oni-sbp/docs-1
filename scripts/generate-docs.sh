1913aea7100c9ffcd9e55642404f44f7d6959774
Ovidiu-SBP <61192792+Ovidiu-SBP@users.noreply.github.com>
2020-02-20 17:05:20 +0200

    Add files via upload

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+git clone https://oni-sbp:$GH_PASSWORD@github.com/oni-sbp/CodeGen-Code.git
+
+cd CodeGen-Code
+npm install
+
+cd docs
+bundle install
+
+cd raml2markdown
+npm install
+
+cd oas-raml-converter
+npm install
+
+node API-Engine-cmd.js --input ./raml2markdown/src --output ./build  --host $HOST --scheme $SCHEME
