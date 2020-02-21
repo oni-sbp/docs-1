796a753b5da8cdd178efb1b08b9d1c87875b46ac
Ovidiu-SBP <61192792+Ovidiu-SBP@users.noreply.github.com>
2020-02-21 15:05:06 +0200

    Update generate-docs.sh

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -6,13 +6,25 @@ git clone https://oni-sbp:$ONI_PASS@github.com/oni-sbp/CodeGen-Code.git
 cd CodeGen-Code
 npm install
 
+cp -r doc-generation/lib ./docs/raml2markdown/oas-raml-converter/
+
 cd docs
 bundle install
 
 cd raml2markdown
 npm install
+npm install swagger-to-slate
 
-cd oas-raml-converter
-npm install
+cd ../..
 
-node API-Engine-cmd.js --input ./docs//raml2markdown/src  --output ./build --host $HOST --scheme $SCHEME
+cp -r doc-generation/src ./docs/raml2markdown/node_modules/swagger-to-slate
+
+mkdir resources/Temp-Files
+
+node API-Engine-cmd.js --input ./docs/raml2markdown/src --output ../build --host $HOST --scheme $SCHEME
+
+cat resources/Temp-Files/*/OAS/acl.json
+
+sudo apt-get update
+sudo apt-get install tree
+tree resources/Temp-Files/
